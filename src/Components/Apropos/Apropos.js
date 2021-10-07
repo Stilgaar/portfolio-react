ef1ff53353cbb125324978a39c4821b2fb75249b
Stilgaar <stilgar182@gmail.com>
2021-10-07 21:11:56 +0200

    le apropos semble a etre pas mal

--- src/Components/Apropos/Apropos.js
+++ src/Components/Apropos/Apropos.js
@@ -7,9 +7,27 @@ function Apropos() {
             <div className="apropElem">
                 <h1 className="titreApropos"> Je<span className="spanApropos">ff</span> <br />
                     van Str<span className="spanApropos">aelen</span> <br />
-                    Develo<span className="spanApropos">ppeur</span> <br /> 
+                    Develo<span className="spanApropos">ppeur</span> <br />
                     Fs <span className="spanApropos">Js</span>
                 </h1>
+            </div>
+            <div className="aproposBox">
+
+                <div className="laboiteatexte">
+                    Salut, <br />
+                    Je m'appelle Jeff, 39 ans, Grasse dans la région PACA. Après avoir
+                    fait mes études de psychologie, je me suis tourné vers
+                    la restauration de luxe. Ensuite, après m'être lancé dans le blogging
+                    voyage, je me suis également professionalisé en photographie en
+                    autodidacte. L'univers digital m'ayant toujours attiré, j'ai fini par
+                    me tourner vers le développement web. J'ai suivi une formation intensive,
+                    dite Bootcamp au Bocal Academy de Nice. Ainsi je suis
+                    devenu développeur full stack en Javascript. <br /><br />
+                    Mes diverses compétences et façons de travailler acquises au fil
+                    des années font de cette nouvelle orientation une super
+                    opportunité très bienvenue.<br />
+                    A très bientôt <br />
+                </div>
             </div>
         </div>
     )
